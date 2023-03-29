8264e623c57720fd79888e3151489b397efb10a8
Liid <51267703+L11D@users.noreply.github.com>
2023-03-29 08:55:44 +0700

    WebGL research is continuing

--- algorithms/Ant algorithm/WebGLFunctions.js
+++ algorithms/Ant algorithm/WebGLFunctions.js
@@ -1,17 +1,101 @@
-export function draw(gl, program, pos, color)
+let rectBuffer;
+
+export function drawRect(gl, program, pos, color)
 {
-    let modelMatrixLocation = gl.getUniformLocation(program, "uModelMatrix");
-    let colorUniformLocation = gl.getUniformLocation(program, "u_color");
-    let modelMatrix = mat4.create();
-    mat4.fromTranslation(modelMatrix, [pos[0], pos[1], 0]);
-    gl.uniformMatrix4fv(modelMatrixLocation, false, modelMatrix);
-    gl.uniform4fv(colorUniformLocation, color);
+  gl.useProgram(program);
+  gl.bindBuffer(gl.ARRAY_BUFFER, rectBuffer);
+
+  let modelMatrixLocation = gl.getUniformLocation(program, "uModelMatrix");
+  let colorUniformLocation = gl.getUniformLocation(program, "u_color");
+  let modelMatrix = mat4.create();
+  mat4.fromTranslation(modelMatrix, [pos[0], pos[1], 0]);
+  gl.uniformMatrix4fv(modelMatrixLocation, false, modelMatrix);
+  gl.uniform4fv(colorUniformLocation, color);
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+    
+}
+
+export function drawField(gl, program)
+{
+    gl.useProgram(program);
+    let arr = new Array(100*100*3);
+
+    for (let i = 0; i < 100*100*3; i++) {
+        if (i % 3 == 0) {
+          arr[i] = 255;
+        }
+        else
+        {
+          arr[i] = 0;
+        }
+    }
+
+    let texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, 100, 100, 0, gl.RGB, gl.UNSIGNED_BYTE, new Uint8Array(arr));
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.generateMipmap(gl.TEXTURE_2D);
+    let textureLocation = gl.getUniformLocation(program, "uTexture");
+    gl.uniform1i(textureLocation, 0);
 
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
     
 }
 
-export function initWebGL(gl, program, width, height, tileSize)
+export function initProgramField(gl, program, width, height)
+{
+    gl.viewport(0, 0, width, height);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    
+    let vertexShaderSource = `
+      attribute vec2 a_position;
+      void main() {
+        gl_Position = vec4(a_position, 0.0, 1.0);
+      }
+    `;
+    
+    let fragmentShaderSource = `
+      precision mediump float;
+      uniform sampler2D uTexture;
+      void main() {
+        vec2 st = gl_FragCoord.xy/100.0;
+	      gl_FragColor = texture2D(uTexture, st);
+      }
+    `;
+    
+    let vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader, vertexShaderSource);
+    gl.compileShader(vertexShader);
+    
+    let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader, fragmentShaderSource);
+    gl.compileShader(fragmentShader);
+    
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    gl.useProgram(program);
+    
+    let positions = new Float32Array([
+      -1, 1, //left up
+      1, 1, //right up
+      -1, -1, //left down
+      1, -1, //rigth down
+    ]);
+    let positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+    let buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(positionAttributeLocation);
+    gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+    
+}
+
+export function initProgramRect(gl, program, width, height, tileSize)
 {
     gl.viewport(0, 0, width, height);
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -29,7 +113,7 @@ export function initWebGL(gl, program, width, height, tileSize)
       precision mediump float;
       uniform vec4 u_color;
       void main() {
-        gl_FragColor = u_color;
+	      gl_FragColor = u_color;
       }
     `;
     
@@ -53,9 +137,10 @@ export function initWebGL(gl, program, width, height, tileSize)
         -1, -1, //left down
         -1+tileSize, -1, //rigth down
     ]);
+    
     let positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-    let buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    rectBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, rectBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
     gl.enableVertexAttribArray(positionAttributeLocation);
     gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
--- algorithms/Ant algorithm/logic.js
+++ algorithms/Ant algorithm/logic.js
@@ -1,28 +1,40 @@
-import {draw, initWebGL} from './WebGLFunctions.js';
+import {drawRect, drawField, initProgramField, initProgramRect} from './WebGLFunctions.js';
 
 
 const canvas = document.querySelector("canvas");
 const gl = canvas.getContext("webgl");
-let program = gl.createProgram();
+let programField = gl.createProgram();
+let programRect = gl.createProgram();
 
 let fieldSize = 100;
 let tileSize = 2/fieldSize;
-initWebGL(gl, program, canvas.width, canvas.height, tileSize);
+initProgramRect(gl, programRect, canvas.width, canvas.height, tileSize);
+initProgramField(gl, programField, canvas.width, canvas.height);
 
+let field = new Array(fieldSize);
+for (let i = 0; i < fieldSize; i++) {
+    field[i] = new Array(fieldSize);  
+    for (let j = 0; j < fieldSize; j++) {
+        field[i][j] = Math.random();        
+    }  
+}
 
-let a = 0;
+let a = 1;
 function update()
 {
     let t0 = performance.now();
     gl.clear(gl.COLOR_BUFFER_BIT);
-    for (let i = 0; i < fieldSize; i++) {
-        for (let j = 0; j < fieldSize; j++) {
-         draw(gl, program, [i*tileSize, j*tileSize], [a,0,0,1]);
-        }
-    }
+    // for (let i = 0; i < fieldSize; i++) {
+    //     for (let j = 0; j < fieldSize; j++) {
+    //         draw(gl, program, [i*tileSize, j*tileSize], [a,0,0,1]);
+    //     }
+    // }
+    drawField(gl, programField)
+    drawRect(gl, programRect, [0.1, 0.1], [1,0,1,1]);
+
     let t1 = performance.now();
     console.log(t1-t0);
     a+=0.01;
-    requestAnimationFrame(update);
+    //requestAnimationFrame(update);
 }
 update();
