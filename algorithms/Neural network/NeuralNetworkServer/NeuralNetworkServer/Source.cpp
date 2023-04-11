cfb9215c83f6f53f813cafd9b5a34a23ab1b5afe
Liid <51267703+L11D@users.noreply.github.com>
2023-04-11 16:22:22 +0700

    network architecture

--- algorithms/Neural network/NeuralNetworkServer/NeuralNetworkServer/Source.cpp
+++ algorithms/Neural network/NeuralNetworkServer/NeuralNetworkServer/Source.cpp
@@ -2,31 +2,74 @@
 #include <vector>
 #include <Eigen/Dense>
 #include <fstream>
+#include <math.h>
+#include"Functions.h"
 using namespace std;
-using namespace Eigen;
+using Eigen::MatrixXd;
 
+double activationFunction(double x)
+{
+	return 1 / (1 + exp(-x));
+}
 
+void applyFunction(MatrixXd& m)
+{
+	for (int i = 0; i < m.rows(); i++)
+	{
+		for (int j = 0; j < m.cols(); j++)
+		{
+			m(i, j) = activationFunction(m(i, j));
+		}
+	}
+}
+
+void createNewWeights(vector<int> layers)
+{
+	for (int i = 1; i < layers.size(); i++)
+	{
+		MatrixXd m = MatrixXd::Random(layers[i], layers[i - 1]);
+		saveMatrix(m, "weights1\\matrix" + to_string(i-1) + "-" + to_string(i) + ".bin");
+	}
+}
+
+void readWeights(vector<MatrixXd>& weightMatrixes)
+{
+	for (int i = 1; i < weightMatrixes.size(); i++)
+	{
+		weightMatrixes[i] = readMatrix("weights\\matrix" + to_string(i - 1) + "-" + to_string(i) + ".bin");
+	}
+}
 
 int main()
 {
-	MatrixXd m(2, 10);
-	m << 1, 2, 3, 4, 5, 6, 7, 8, 9, 10;
-	std::cout << m << std::endl;
+	//std::filesystem::path path_to_dir = "."; // путь до каталога
+	//for (const auto& entry : std::filesystem::directory_iterator(path_to_dir)) {
+	//	if (entry.is_directory()) {
+	//		std::cout << entry.path() << std::endl;
+	//	}
+	//}
+		
+	/*int inputSize = 50;
+	vector<int> layers = { inputSize, 32, 16, 10 };
+	vector<MatrixXd> weightMatrixes(layers.size());
 
-	cout << endl << m.size();
-	
-	//return 0;
-	//std::ofstream file("matrix.bin", std::ios::out | std::ios::binary);
-	//file.write((char*)m.data(), m.size() * sizeof(double));
-	//file.close();
+	createNewWeights(layers);
 
-	//MatrixXd n(2, 2);
-	//std::ifstream infile("matrix.bin", std::ios::binary);
-	//infile.read((char*)n.data(), n.size() * sizeof(double));
-	//infile.close();
+	readWeights(weightMatrixes);
 
-	//cout << endl;
-	//cout << n;
+	MatrixXd inputMatrix(inputSize, 1);
+	for (int i = 0; i < inputSize; i++)
+	{
+		inputMatrix(i, 0) = i;
+	}
 
-	//std::system("pause");
+	MatrixXd currentMatrix = inputMatrix;
+	for (int i = 1; i < layers.size(); i++)
+	{
+		currentMatrix = weightMatrixes[i] * currentMatrix;
+		applyFunction(currentMatrix);
+		cout << currentMatrix << endl << endl;
+	}*/
+
+	system("pause");
 }
