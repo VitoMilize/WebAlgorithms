aa65d9791aa34457fd385a424c78f9353e415c7a
Liid <51267703+L11D@users.noreply.github.com>
2023-04-11 22:20:49 +0700

    filesystem

--- algorithms/Neural network/NeuralNetworkServer/NeuralNetworkServer/Source.cpp
+++ algorithms/Neural network/NeuralNetworkServer/NeuralNetworkServer/Source.cpp
@@ -3,6 +3,7 @@
 #include <Eigen/Dense>
 #include <fstream>
 #include <math.h>
+#include <filesystem>
 #include"Functions.h"
 using namespace std;
 using Eigen::MatrixXd;
@@ -25,42 +26,61 @@ void applyFunction(MatrixXd& m)
 
 void createNewWeights(vector<int> layers)
 {
+	int lastWeights = 0;
+	filesystem::path pathToWeightsDir = ".//weights";
+	for (const auto& entry : filesystem::directory_iterator(pathToWeightsDir)) {
+		if (entry.is_directory()) {
+			lastWeights = stoi(entry.path().filename().string());
+		}
+	}
+	filesystem::path pathToNewWeightsDir = pathToWeightsDir.string() + "//" + to_string(lastWeights + 1) + "//";
+
+	filesystem::create_directory(pathToNewWeightsDir);
 	for (int i = 1; i < layers.size(); i++)
 	{
 		MatrixXd m = MatrixXd::Random(layers[i], layers[i - 1]);
-		saveMatrix(m, "weights1\\matrix" + to_string(i-1) + "-" + to_string(i) + ".bin");
+		saveMatrix(m, pathToNewWeightsDir.string() + to_string(i - 1) + "-" + to_string(i) + ".bin");
 	}
 }
 
 void readWeights(vector<MatrixXd>& weightMatrixes)
 {
+	int lastWeights = 0;
+	filesystem::path pathToWeightsDir = ".//weights";
+	for (const auto& entry : filesystem::directory_iterator(pathToWeightsDir)) {
+		if (entry.is_directory()) {
+			lastWeights = stoi(entry.path().filename().string());
+		}
+	}
+
+	if (lastWeights == 0)
+	{
+		cout << "weights not found" << endl;
+		return;
+	}
+
+	filesystem::path pathToLastWeightsDir = pathToWeightsDir.string() + "//" + to_string(lastWeights) + "//";
+
 	for (int i = 1; i < weightMatrixes.size(); i++)
 	{
-		weightMatrixes[i] = readMatrix("weights\\matrix" + to_string(i - 1) + "-" + to_string(i) + ".bin");
+		weightMatrixes[i] = readMatrix(pathToLastWeightsDir.string() + to_string(i - 1) + "-" + to_string(i) + ".bin");
 	}
 }
 
 int main()
-{
-	//std::filesystem::path path_to_dir = "."; // путь до каталога
-	//for (const auto& entry : std::filesystem::directory_iterator(path_to_dir)) {
-	//	if (entry.is_directory()) {
-	//		std::cout << entry.path() << std::endl;
-	//	}
-	//}
-		
-	/*int inputSize = 50;
+{		
+	int inputSize = 50;
 	vector<int> layers = { inputSize, 32, 16, 10 };
 	vector<MatrixXd> weightMatrixes(layers.size());
 
-	createNewWeights(layers);
+	//createNewWeights(layers);
 
 	readWeights(weightMatrixes);
 
 	MatrixXd inputMatrix(inputSize, 1);
 	for (int i = 0; i < inputSize; i++)
 	{
-		inputMatrix(i, 0) = i;
+		inputMatrix(i, 0) = 1;
 	}
 
 	MatrixXd currentMatrix = inputMatrix;
@@ -69,7 +89,7 @@ int main()
 		currentMatrix = weightMatrixes[i] * currentMatrix;
 		applyFunction(currentMatrix);
 		cout << currentMatrix << endl << endl;
-	}*/
+	}
 
 	system("pause");
 }
