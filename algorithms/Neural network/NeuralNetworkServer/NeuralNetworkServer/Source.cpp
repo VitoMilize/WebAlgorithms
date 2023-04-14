65c06968af606520f1cca311f54f506cd43893d8
Liid <51267703+L11D@users.noreply.github.com>
2023-04-14 20:23:09 +0700

    it can lern

--- algorithms/Neural network/NeuralNetworkServer/NeuralNetworkServer/Source.cpp
+++ algorithms/Neural network/NeuralNetworkServer/NeuralNetworkServer/Source.cpp
@@ -4,17 +4,19 @@
 #include <fstream>
 #include <math.h>
 #include <filesystem>
+#include <rapidcsv.h>
 #include"Functions.h"
 #include <omp.h>
 using namespace std;
 using Eigen::MatrixXd;
 
-int inputSize = 28;
+int inputSize = 28 * 28;
 vector<int> layers = { inputSize, 32, 16, 10 };
 vector<MatrixXd> weightMatrixes(layers.size());
-vector<MatrixXd> neuronSums(layers.size());
+vector<MatrixXd> neuronInputs(layers.size());
 vector<MatrixXd> neuronOutputs(layers.size());
 vector<MatrixXd> neuronErrors(layers.size());
+vector<MatrixXd> neuronErrorsMultWeights(layers.size());
 
 double sigmoid(double x)
 {
@@ -80,99 +82,147 @@ void readWeights(vector<MatrixXd>& weightMatrixes)
 	}
 }
 
+void saveWeights(vector<MatrixXd>& weightMatrixes)
+{
+	int lastWeights = 0;
+	filesystem::path pathToWeightsDir = ".//weights";
+	for (const auto& entry : filesystem::directory_iterator(pathToWeightsDir)) {
+		if (entry.is_directory()) {
+			lastWeights = stoi(entry.path().filename().string());
+		}
+	}
+	filesystem::path pathToLastWeightsDir = pathToWeightsDir.string() + "//" + to_string(lastWeights) + "//";
+}
+
 void directPassage(MatrixXd inputMatrix)
 {
 	neuronOutputs[0] = inputMatrix;
 	for (int i = 1; i < layers.size(); i++)
 	{
 		//cout << "neuronOutputs" << i - 1 << ":" << endl << neuronOutputs[i - 1] << endl << endl;
-		neuronSums[i] = weightMatrixes[i] * neuronOutputs[i - 1];
+		neuronInputs[i] = weightMatrixes[i] * neuronOutputs[i - 1];
 		//cout << "weights" << i - 1 << "-" << i << ":" << endl << weightMatrixes[i] << endl << endl;
 		//cout << "neuronSums" << i << ":" << endl << neuronSums[i] << endl << endl;
-		neuronOutputs[i] = neuronSums[i];
+		neuronOutputs[i] = neuronInputs[i];
 		applyFunctionToMatrix(neuronOutputs[i], sigmoid);
 	}
 }
 
+void lern(MatrixXd inputMatrix, MatrixXd rightAnswers)
+{
+	for (int i = layers.size() - 1; i > 0; i--)
+	{
+		//cout << "layer: " << i << endl;
+		//cout << weightMatrixes[i] << endl << endl;
+		MatrixXd oldWeights = weightMatrixes[i];
+#pragma omp parallel for
+		for (int j = 0; j < neuronOutputs[i].size(); j++)
+		{
+			//cout << "\tneuron: " << j << endl;
+			double error;
+			if (i == layers.size() - 1)
+			{
+				error = -2 * (neuronOutputs[i](j, 0) - rightAnswers(j, 0)) * sigmoidD(neuronInputs[i](j, 0));
+				//error = -2 * (neuronOutputs[i](j, 0) - rightAnswers(j, 0)) * sigmoidD(neuronSums[i](j, 0));
+				/*cout << "\t\t neuron output: " << neuronOutputs[i](j, 0) << endl;
+				cout << "\t\t right answer: " << rightAnswers(j, 0) << endl;
+				cout << "\t\t neuron input: " << neuronInputs[i](j, 0) << endl;
+				cout << "\t\t sigmoidD: " << sigmoidD(neuronInputs[i](j, 0)) << endl;
+				cout << "\t\t error: " << error << endl;*/
+			}
+			else
+			{
+				error = neuronErrorsMultWeights[i + 1](j, 0) * sigmoidD(neuronInputs[i](j, 0));
+				/*cout << "\t\t neuron output: " << neuronOutputs[i](j, 0) << endl;
+				cout << "\t\t neuron errors mult weights: " << neuronErrorsMultWeights[i + 1](j, 0) << endl;
+				cout << "\t\t neuron input: " << neuronInputs[i](j, 0) << endl;
+				cout << "\t\t sigmoidD: " << sigmoidD(neuronInputs[i](j, 0)) << endl;
+				cout << "\t\t error: " << error << endl;*/
+			}
+			neuronErrors[i](j, 0) = error;
+			for (int w = 0; w < weightMatrixes[i].cols(); w++)
+			{
+				//cout << "\t\t pre neuron output" << w << ": " << neuronOutputs[i - 1](w, 0);
+				double Dw = error * neuronOutputs[i - 1](w, 0);
+				//cout << "\t\t Dw" << w << ": " << Dw << endl;
+				weightMatrixes[i](j, w) += Dw;
+			}
+		}
+		//cout << neuronErrors[i] << endl << endl;
+		//cout << oldWeights.transpose() << endl << endl;
+		//cout << neuronErrorsMultWeights[i] << endl << endl;
+
+		//neuronErrorsMultWeights[i] = weightMatrixes[i].transpose() * neuronErrors[i];
+		neuronErrorsMultWeights[i] = oldWeights.transpose() * neuronErrors[i];
+
+		//cout << weightMatrixes[i] << endl << endl;
+	}
+	directPassage(inputMatrix);
+}
+
+void readDataset(rapidcsv::Document& doc, MatrixXd& inputMatrix, MatrixXd& rightAnswers)
+{
+
+}
+
 int main()
-{		
-	/*vector<int> layers = { inputSize, 32, 16, 10 };*/
+{
+	rapidcsv::Document doc("./dataset//mnist_train.csv");
+	for (int f = 0; f < 50; f++)
+	{
+		vector<double> row = doc.GetRow<double>(f);
+		cout << row[0] << endl;
+		for (int i = 1; i < row.size(); i++)
+		{
+			if (row[i] != 0)
+			{
+				cout << "1 ";
+			}
+			else
+			{
+				cout << "0 ";
+			}
+			if ((i) % 28 == 0)
+			{
+				cout << endl;
+			}
+		}
+		cout << endl;
+	}
 	
 
 	//createNewWeights(layers);
 
-	readWeights(weightMatrixes);
+	//readWeights(weightMatrixes);
 
-	MatrixXd inputMatrix(inputSize, 1);
-	for (int i = 0; i < inputSize; i++)
-	{
-		inputMatrix(i, 0) = 1;
-	}
+	//
 
-	MatrixXd rightAnswers(layers[layers.size()-1], 1);
-	for (int i = 0; i < rightAnswers.size(); i++)
-	{
-		rightAnswers(i, 0) = 0;
-	}
-	rightAnswers(0, 0) = 1;
-	directPassage(inputMatrix);
+	//MatrixXd inputMatrix(inputSize, 1);
+	//for (int i = 0; i < inputSize; i++)
+	//{
+	//	inputMatrix(i, 0) = 1;
+	//}
 
-	for (int i = 0; i < layers.size(); i++)
-	{
-		neuronErrors[i] = MatrixXd(layers[i], 1);
-	}
+	//MatrixXd rightAnswers(layers[layers.size() - 1], 1);
+	//for (int i = 0; i < rightAnswers.size(); i++)
+	//{
+	//	rightAnswers(i, 0) = 1;
+	//}
 
-	//cout << neuronOutputs[neuronOutputs.size() - 1] << endl << endl;
+	//directPassage(inputMatrix);
 
-	for (int k = 0; k < 100000000; k++)
-	{
-		for (int i = layers.size() - 1; i > 0; i--)
-		{
-#pragma omp parallel for
-			for (int j = 0; j < neuronOutputs[i].size(); j++)
-			{
-				//cout << "neuron: " << j << endl;
-				double error;
-				if (i == layers.size() - 1)
-				{
-					error = -2 * (neuronOutputs[i](j, 0) - rightAnswers(j, 0)) * sigmoidD(neuronSums[i](j, 0));
-					//error = -2 * (neuronOutputs[i](j, 0) - rightAnswers(j, 0)) * sigmoidD(neuronSums[i](j, 0));
-					/*cout << "\t neuron output: " << neuronOutputs[i](j, 0) << endl;
-					cout << "\t right answer: " << rightAnswers(j, 0) << endl;
-					cout << "\t neuron sum: " << neuronSums[i](j, 0) << endl;
-					cout << "\t sigmoidD: " << sigmoidD(neuronSums[i](j, 0)) << endl;
-					cout << "\t error: " << error << endl;*/
-				}
-				else
-				{
-					error = -2 * (neuronErrors[i](j,0)) * sigmoidD(neuronSums[i](j, 0));
-				}
-				neuronErrors[i](j, 0) = error;
-				for (int w = 0; w < weightMatrixes[i].cols(); w++)
-				{
-					//cout << "\t\t pre neuron output" << w << ": " << neuronOutputs[i - 1](w, 0);
-					double Dw = error * neuronOutputs[i - 1](w, 0);
-					//cout << "\t\t Dw" << w << ": " << Dw << endl;
-					weightMatrixes[i](w, 0) += Dw;
-				}
-			}
-			neuronErrors[i] = weightMatrixes[i].transpose() * neuronErrors[i];
-		}
-		directPassage(inputMatrix);
+	//for (int i = 0; i < layers.size(); i++)
+	//{
+	//	neuronErrors[i] = MatrixXd::Ones(layers[i], 1);
+	//}
 
-		if (k % 10000 == 0)
-			cout << neuronOutputs[neuronOutputs.size() - 1] << endl << endl;
-	}
 
-	neuronOutputs[0] = inputMatrix;
-	for (int i = 1; i < layers.size(); i++)
-	{
-		neuronSums[i] = weightMatrixes[i] * neuronOutputs[i - 1];
-		neuronOutputs[i] = neuronSums[i];
-		applyFunctionToMatrix(neuronOutputs[i], sigmoid);
-	}
+	//for (int k = 0; k < 1000000; k++)
+	//{
+	//	lern(inputMatrix, rightAnswers);
+	//	if (k % 10000 == 0)
+	//		cout << neuronOutputs[neuronOutputs.size() - 1] << endl << endl;
+	//}
 
-	//cout << neuronOutputs[neuronOutputs.size() - 1] << endl;
-
-	system("pause");
+	std::system("pause");
 }
