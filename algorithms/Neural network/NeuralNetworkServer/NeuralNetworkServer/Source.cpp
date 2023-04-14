de0f293b14e638f1c94153d530e3d728a19f4f5d
Liid <51267703+L11D@users.noreply.github.com>
2023-04-14 08:54:09 +0700

    testing 4

--- algorithms/Neural network/NeuralNetworkServer/NeuralNetworkServer/Source.cpp
+++ algorithms/Neural network/NeuralNetworkServer/NeuralNetworkServer/Source.cpp
@@ -5,8 +5,16 @@
 #include <math.h>
 #include <filesystem>
 #include"Functions.h"
+#include <omp.h>
 using namespace std;
 using Eigen::MatrixXd;
+
+int inputSize = 28;
+vector<int> layers = { inputSize, 32, 16, 10 };
+vector<MatrixXd> weightMatrixes(layers.size());
+vector<MatrixXd> neuronSums(layers.size());
+vector<MatrixXd> neuronOutputs(layers.size());
+vector<MatrixXd> neuronErrors(layers.size());
 
 double sigmoid(double x)
 {
@@ -72,15 +80,24 @@ void readWeights(vector<MatrixXd>& weightMatrixes)
 	}
 }
 
+void directPassage(MatrixXd inputMatrix)
+{
+	neuronOutputs[0] = inputMatrix;
+	for (int i = 1; i < layers.size(); i++)
+	{
+		//cout << "neuronOutputs" << i - 1 << ":" << endl << neuronOutputs[i - 1] << endl << endl;
+		neuronSums[i] = weightMatrixes[i] * neuronOutputs[i - 1];
+		//cout << "weights" << i - 1 << "-" << i << ":" << endl << weightMatrixes[i] << endl << endl;
+		//cout << "neuronSums" << i << ":" << endl << neuronSums[i] << endl << endl;
+		neuronOutputs[i] = neuronSums[i];
+		applyFunctionToMatrix(neuronOutputs[i], sigmoid);
+	}
+}
+
 int main()
 {		
-	int inputSize = 5;
 	/*vector<int> layers = { inputSize, 32, 16, 10 };*/
-	vector<int> layers = { inputSize, 5, 2 };
-	vector<MatrixXd> weightMatrixes(layers.size());
-	vector<MatrixXd> neuronSums(layers.size());
-	vector<MatrixXd> neuronOutputs(layers.size());
-	vector<MatrixXd> neuronErrors(layers.size());
+	
 
 	//createNewWeights(layers);
 
@@ -95,19 +112,10 @@ int main()
 	MatrixXd rightAnswers(layers[layers.size()-1], 1);
 	for (int i = 0; i < rightAnswers.size(); i++)
 	{
-		rightAnswers(i, 0) = 1;
+		rightAnswers(i, 0) = 0;
 	}
-
-	neuronOutputs[0] = inputMatrix;
-	for (int i = 1; i < layers.size(); i++)
-	{
-		cout << "neuronOutputs" << i - 1 << ":" << endl << neuronOutputs[i - 1] << endl << endl;
-		neuronSums[i] = weightMatrixes[i] * neuronOutputs[i - 1];
-		cout << "weights" << i - 1 << "-" << i << ":" << endl << weightMatrixes[i] << endl << endl;
-		cout << "neuronSums" << i << ":" << endl << neuronSums[i] << endl << endl;
-		neuronOutputs[i] = neuronSums[i];
-		applyFunctionToMatrix(neuronOutputs[i], sigmoid);
-	}
+	rightAnswers(0, 0) = 1;
+	directPassage(inputMatrix);
 
 	for (int i = 0; i < layers.size(); i++)
 	{
@@ -116,16 +124,24 @@ int main()
 
 	//cout << neuronOutputs[neuronOutputs.size() - 1] << endl << endl;
 
-	//for (int k = 0; k < 1000; k++)
+	for (int k = 0; k < 100000000; k++)
 	{
 		for (int i = layers.size() - 1; i > 0; i--)
 		{
+#pragma omp parallel for
 			for (int j = 0; j < neuronOutputs[i].size(); j++)
 			{
+				//cout << "neuron: " << j << endl;
 				double error;
 				if (i == layers.size() - 1)
 				{
-					error = -2 * (rightAnswers(j, 0) - neuronOutputs[i](j, 0)) * sigmoidD(neuronSums[i](j, 0));
+					error = -2 * (neuronOutputs[i](j, 0) - rightAnswers(j, 0)) * sigmoidD(neuronSums[i](j, 0));
+					//error = -2 * (neuronOutputs[i](j, 0) - rightAnswers(j, 0)) * sigmoidD(neuronSums[i](j, 0));
+					/*cout << "\t neuron output: " << neuronOutputs[i](j, 0) << endl;
+					cout << "\t right answer: " << rightAnswers(j, 0) << endl;
+					cout << "\t neuron sum: " << neuronSums[i](j, 0) << endl;
+					cout << "\t sigmoidD: " << sigmoidD(neuronSums[i](j, 0)) << endl;
+					cout << "\t error: " << error << endl;*/
 				}
 				else
 				{
@@ -134,12 +150,18 @@ int main()
 				neuronErrors[i](j, 0) = error;
 				for (int w = 0; w < weightMatrixes[i].cols(); w++)
 				{
+					//cout << "\t\t pre neuron output" << w << ": " << neuronOutputs[i - 1](w, 0);
 					double Dw = error * neuronOutputs[i - 1](w, 0);
+					//cout << "\t\t Dw" << w << ": " << Dw << endl;
 					weightMatrixes[i](w, 0) += Dw;
 				}
 			}
 			neuronErrors[i] = weightMatrixes[i].transpose() * neuronErrors[i];
 		}
+		directPassage(inputMatrix);
+
+		if (k % 10000 == 0)
+			cout << neuronOutputs[neuronOutputs.size() - 1] << endl << endl;
 	}
 
 	neuronOutputs[0] = inputMatrix;
