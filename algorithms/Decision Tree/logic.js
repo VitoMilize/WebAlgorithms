3b0bd597ad203cd8a6eb60f6574a0ccc3eee2512
OG-UZOK <karla-an@mail.ru>
2023-04-19 23:59:31 +0700

    Сделал но тут тотальный пиздец

--- algorithms/Decision Tree/logic.js
+++ algorithms/Decision Tree/logic.js
@@ -8,6 +8,7 @@ function buildTree(data, parent) {
   data.forEach(function (item) {
     var li = document.createElement('li');
     li.textContent = item.title + ': ' + item.value;  // здесь изменено свойство, заданное при создании узла
+    //li.textContent = item.value;
     ul.appendChild(li);
     if (item.children && item.children.length) {
       buildTree(item.children, li);
@@ -16,7 +17,7 @@ function buildTree(data, parent) {
 }
 
 function csvToTree(csv) {
-  csv = 'csv,' + csv;//тут тотальный колхоз
+  csv = csv;
   var lines = csv.split("\n");
   var headers = lines[0].split(",").map(function (header) { return header.trim(); });
   var root = { title: headers[0], children: [] };
@@ -26,12 +27,12 @@ function csvToTree(csv) {
     for (var j = 0; j < cols.length; j++) {
       var col = cols[j];
       var existingChild = currentNode.children.find(function (child) {
-        return child.title === headers[j + 1] && child.value === col;
+        return child.title === headers[j] && child.value === col;
       });
       if (existingChild) {
         currentNode = existingChild;
       } else {
-        var newChild = { title: headers[j + 1], value: col, children: [] };
+        var newChild = { title: headers[j], value: col, children: [] };
         currentNode.children.push(newChild);
         currentNode = newChild;
       }
@@ -40,52 +41,6 @@ function csvToTree(csv) {
   return root.children;
 }
 
-function findPathByValue(tree, value, highlight) {
-  var path = [];
-  function search(node) {
-    console.log(node.value);
-    if (node.value === value) {
-      path.push(node);
-      return true;
-    } else if (node.children) {
-      for (var i = 0; i < node.children.length; i++) {
-        if (search(node.children[i])) {
-          path.push(node);
-          return true;
-        }
-      }
-    }
-    return false;
-  }
-  if (highlight) {
-    removeHighlights(tree);
-  }
-  if (search(tree)) {
-    path.reverse();
-    return path;
-  } else {
-    return null;
-  }
-}
-
-
-function removeHighlights(tree) {
-  function traverse(node) {
-    if (node.highlighted) {
-      node.highlighted = false;
-      var li = document.querySelector('li[data-path="' + node.path.join('-') + '"]');
-      if (li) {
-        li.classList.remove('highlighted');
-      }
-    }
-    if (node.children) {
-      for (var i = 0; i < node.children.length; i++) {
-        traverse(node.children[i]);
-      }
-    }
-  }
-  traverse(tree);
-}
 
 
 document.querySelector('#buildTreeBtn').addEventListener('click', function () {
@@ -94,17 +49,3 @@ document.querySelector('#buildTreeBtn').addEventListener('click', function () {
   document.querySelector('#tree').innerHTML = '';
   buildTree(data);
 });
-
-document.querySelector('#searchBtn').addEventListener('click', function () {
-  var searchValue = document.querySelector('#searchInput').value;
-  var data = document.querySelector('#csvInput').value;
-  var tree = csvToTree(data);
-  console.log(searchValue);
-  var path = findPathByValue(tree, searchValue, true);
-  if (path) {
-    highlightPath(path);
-  } else {
-    alert('Element not found');
-  }
-});
-
